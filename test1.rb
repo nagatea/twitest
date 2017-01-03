77854d8a3747c0883393fc0095fdc9bd765516c1
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-03 22:31:54 +0900

    ruby1.rb upgrade

--- test1.rb
+++ test1.rb
@@ -1,13 +1,28 @@
-# coding: utf-8
-require "twitter"
-require "oauth"
+#!/usr/bin/env ruby
 
-client = Twitter::REST::Client.new(
-  consumer_key:        "C8MO23D1Yvn4rerE4IBapJIoJ",
-  consumer_secret:     "2jh7fsh4d9d85iTyKNXTVBb0KpkWv8qJmWoMbVdHClgnBoK3Rh",
-  access_token:        "736847305526018048-PWAZ9eKtoDAtr8Ki7rFBiNV7Vk9zVcn",
-  access_token_secret: "Su4l7LxnMQ4S3mgCUYRa2UUxLtJI3uYHbpY7K7sdnkOBF",
+require 'rubygems'
+require 'rubytter'
+require 'oauth'
+
+CONSUMER_KEY = 'C8MO23D1Yvn4rerE4IBapJIoJ'
+CONSUMER_SERCRET = '2jh7fsh4d9d85iTyKNXTVBb0KpkWv8qJmWoMbVdHClgnBoK3Rh'
+
+ACCESS_TOKEN = '736847305526018048-PWAZ9eKtoDAtr8Ki7rFBiNV7Vk9zVcn'
+ACCESS_TOKEN_SERCTET = 'Su4l7LxnMQ4S3mgCUYRa2UUxLtJI3uYHbpY7K7sdnkOBF'
+
+consumer = OAuth::Consumer.new(
+  CONSUMER_KEY,
+  CONSUMER_SERCRET,
+  :site => 'http://api.twitter.com'
 )
 
-client.update("test")
+access_token = OAuth::AccessToken.new(
+  consumer,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SERCTET
+)
+
+rubytter = OAuthRubytter.new(access_token)
+
+rubytter.update("てすと")
 puts("投稿しました！")
