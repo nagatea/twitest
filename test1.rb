70db9a755d1e92be59b69daf63822b7c58866b01
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-05 03:26:42 +0900

    test1,rbを更新しました。投稿テストにも成功！

--- test1.rb
+++ test1.rb
@@ -1,28 +1,13 @@
-#!/usr/bin/env ruby
+require "twitter"
 
-require 'rubygems'
-require 'rubytter'
-require 'oauth'
+ENV['SSL_CERT_FILE'] = File.expand_path('C:\tools\cacert.pem')
 
-CONSUMER_KEY = 'C8MO23D1Yvn4rerE4IBapJIoJ'
-CONSUMER_SERCRET = '2jh7fsh4d9d85iTyKNXTVBb0KpkWv8qJmWoMbVdHClgnBoK3Rh'
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "C8MO23D1Yvn4rerE4IBapJIoJ"
+  config.consumer_secret     = "2jh7fsh4d9d85iTyKNXTVBb0KpkWv8qJmWoMbVdHClgnBoK3Rh"
+  config.access_token        = "736847305526018048-PWAZ9eKtoDAtr8Ki7rFBiNV7Vk9zVcn"
+  config.access_token_secret = "Su4l7LxnMQ4S3mgCUYRa2UUxLtJI3uYHbpY7K7sdnkOBF"
+end
 
-ACCESS_TOKEN = '736847305526018048-PWAZ9eKtoDAtr8Ki7rFBiNV7Vk9zVcn'
-ACCESS_TOKEN_SERCTET = 'Su4l7LxnMQ4S3mgCUYRa2UUxLtJI3uYHbpY7K7sdnkOBF'
-
-consumer = OAuth::Consumer.new(
-  CONSUMER_KEY,
-  CONSUMER_SERCRET,
-  :site => 'http://api.twitter.com'
-)
-
-access_token = OAuth::AccessToken.new(
-  consumer,
-  ACCESS_TOKEN,
-  ACCESS_TOKEN_SERCTET
-)
-
-rubytter = OAuthRubytter.new(access_token)
-
-rubytter.update("てすと")
+client.update("てすと")
 puts("投稿しました！")
