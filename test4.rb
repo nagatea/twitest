aa05f65f0ed76a71e1edae6b3f17a99229ad263a
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-05 21:56:35 +0900

    同じ相手に何度も送れないことにむかついたので、投稿した時間を付け加えて重複投稿を防ぎました

--- test4.rb
+++ test4.rb
@@ -12,10 +12,18 @@ end
 
 user_id = ARGV[0].to_s
 
+time = Time.now
+y = time.year
+mon = time.month
+d = time.day
+h = time.hour
+min = time.min
+s = time.sec
+
 puts("読み込み完了")
 
 if user_id != ""
-    client.update("@#{user_id} FF外から失礼するゾ～（謝罪） このツイート面白スギィ！！！！！自分、RTいいっすか？ 淫夢知ってそうだから淫夢のリストにぶち込んでやるぜー いきなりリプしてすみません！許してください！なんでもしますから！(なんでもするとは言ってない)")
+    client.update("@#{user_id} FF外から失礼するゾ～（謝罪） このツイート面白スギィ！！！！！自分、RTいいっすか？ 淫夢知ってそうだから淫夢のリストにぶち込んでやるぜー いきなりリプしてすみません！許してください！なんでもしますから！(なんでもするとは言ってない)\n#{y}#{mon}#{d}#{h}#{min}#{s}")
     puts("投稿しました！")
 else
     puts("[EROOR]@以下を入力してください")
