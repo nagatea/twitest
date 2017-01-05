67303878753fab88bd010ed6bd537016857b3a81
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-05 14:19:49 +0900

    トークン別管理形式に変更(もちろんトークンは作り直した)

--- test3.rb
+++ test3.rb
@@ -1,13 +1,15 @@
 require "twitter"
+require "./mysystem.rb"
 
 ENV['SSL_CERT_FILE'] = File.expand_path('C:\tools\cacert.pem')
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "C8MO23D1Yvn4rerE4IBapJIoJ"
-  config.consumer_secret     = "2jh7fsh4d9d85iTyKNXTVBb0KpkWv8qJmWoMbVdHClgnBoK3Rh"
-  config.access_token        = "736847305526018048-PWAZ9eKtoDAtr8Ki7rFBiNV7Vk9zVcn"
-  config.access_token_secret = "Su4l7LxnMQ4S3mgCUYRa2UUxLtJI3uYHbpY7K7sdnkOBF"
+  config.consumer_key        = MY_CONSUMER_KEY
+  config.consumer_secret     = MY_CONSUMER_SECRET
+  config.access_token        = MY_ACCESS_TOKEN
+  config.access_token_secret = MY_ACCESS_TOKEN_SECRET
 end
+puts("読み込み完了")
 
 user_id = ARGV[0]
 
