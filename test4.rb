7dbb681de0cb572a898ea822d64c9d493c0b6476
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-05 23:28:40 +0900

    投稿時間の処理をtime.rbでするように変更

--- test4.rb
+++ test4.rb
@@ -1,5 +1,6 @@
 require "twitter"
 require "./mysystem.rb"
+require "./time.rb"
 
 ENV['SSL_CERT_FILE'] = File.expand_path('C:\tools\cacert.pem')
 
@@ -12,19 +13,17 @@ end
 
 user_id = ARGV[0].to_s
 
-time = Time.now
-y = time.year
-mon = time.month
-d = time.day
-h = time.hour
-min = time.min
-s = time.sec
-
 puts("読み込み完了")
 
+puts("ーーー投稿予定内容ーーー\n@#{user_id} FF外から失礼するゾ～（謝罪） このツイート面白スギィ！！！！！自分、RTいいっすか？ 淫夢知ってそうだから淫夢のリストにぶち込んでやるぜー いきなりリプしてすみません！許してください！なんでもしますから！(なんでもするとは言ってない)\n"+ TIME_CODE + "\nーーーーーーーーーーーー")
+
 if user_id != ""
-    client.update("@#{user_id} FF外から失礼するゾ～（謝罪） このツイート面白スギィ！！！！！自分、RTいいっすか？ 淫夢知ってそうだから淫夢のリストにぶち込んでやるぜー いきなりリプしてすみません！許してください！なんでもしますから！(なんでもするとは言ってない)\n#{y}#{mon}#{d}#{h}#{min}#{s}")
-    puts("投稿しました！")
+    if user_id.size < 10
+        client.update("@#{user_id} FF外から失礼するゾ～（謝罪） このツイート面白スギィ！自分、RTいいっすか？ 淫夢知ってそうだから淫夢のリストにぶち込んでやるぜー いきなりリプしてすみません！許してください！なんでもしますから！(なんでもするとは言ってない)\n" + TIME_CODE)
+        puts("投稿しました！")
+    else
+        puts("[EROOR]文字数オーバーです")
+    end
 else
     puts("[EROOR]@以下を入力してください")
 end
