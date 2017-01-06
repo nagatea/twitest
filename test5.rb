d2f6e434fd1d5f0e1b5487cf9854200a2dd88b42
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-06 17:32:45 +0900

    指定したIDの人をフォローするコード(test5.rb)の作成

---
+++ test5.rb
@@ -0,0 +1,16 @@
+require "twitter"
+require "./mysystem.rb"
+
+ENV['SSL_CERT_FILE'] = File.expand_path('C:\tools\cacert.pem')
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = MY_CONSUMER_KEY
+  config.consumer_secret     = MY_CONSUMER_SECRET
+  config.access_token        = MY_ACCESS_TOKEN
+  config.access_token_secret = MY_ACCESS_TOKEN_SECRET
+end
+puts("読み込み完了")
+
+client.follow(ARGV[0])
+client.update("@#{ARGV[0]}をフォローしました")
+puts("@#{ARGV[0]}をフォローしました")
