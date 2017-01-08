2ed681314e2e3504bf0db7178ec73d9c79d65798
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-09 02:39:42 +0900

    ちょっと修正

--- test/test5.rb
+++ test/test5.rb
@@ -15,7 +15,7 @@ if ARGV[1].to_s == "follow"
   client.follow(ARGV[0])
   client.update("#{ARGV[0]} をフォローしました")
   puts("@#{ARGV[0]}をフォローしました")
-elsif ARGV[1] == "unfollow"
+elsif ARGV[1].to_s == "unfollow"
   client.unfollow(ARGV[0])
   client.update("#{ARGV[0]} をリムりました")
   puts("@#{ARGV[0]}をリムりました")
