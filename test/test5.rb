345428bc2a372f98ed1781f223257644aa2ff90b
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-09 02:04:10 +0900

    リムーブ(unfollow)も出来るように変更

--- test/test5.rb
+++ test/test5.rb
@@ -1,7 +1,7 @@
 require "twitter"
-require "./mysystem.rb"
+require "../mysystem.rb"
 
-ENV['SSL_CERT_FILE'] = File.expand_path('C:\tools\cacert.pem')
+ENV['SSL_CERT_FILE'] = File.expand_path('../cacert.pem')
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = MY_CONSUMER_KEY
@@ -11,6 +11,14 @@ client = Twitter::REST::Client.new do |config|
 end
 puts("読み込み完了")
 
-client.follow(ARGV[0])
-client.update("@#{ARGV[0]}をフォローしました")
-puts("@#{ARGV[0]}をフォローしました")
+if ARGV[1].to_s == "follow"
+  client.follow(ARGV[0])
+  client.update("#{ARGV[0]} をフォローしました")
+  puts("@#{ARGV[0]}をフォローしました")
+elsif ARGV[1] == "unfollow"
+  client.unfollow(ARGV[0])
+  client.update("#{ARGV[0]} をリムりました")
+  puts("@#{ARGV[0]}をリムりました")
+else
+  puts("[ERROR]IDとfollow or unfollowを正しく入力してください")
+end
