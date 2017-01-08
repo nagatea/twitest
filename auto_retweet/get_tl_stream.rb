65a36580435eae99bfd4f026cdcb17acd9c8dffb
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-08 16:57:45 +0900

    streamAPIを用いたTLを取得するコード(get_tl_stream.rb)の作成

---
+++ auto_retweet/get_tl_stream.rb
@@ -0,0 +1,32 @@
+require "twitter"
+require "../mysystem.rb"
+
+ENV['SSL_CERT_FILE'] = File.expand_path('../cacert.pem')
+
+stream_client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = MY_CONSUMER_KEY
+  config.consumer_secret     = MY_CONSUMER_SECRET
+  config.access_token        = MY_ACCESS_TOKEN
+  config.access_token_secret = MY_ACCESS_TOKEN_SECRET
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = MY_CONSUMER_KEY
+  config.consumer_secret     = MY_CONSUMER_SECRET
+  config.access_token        = MY_ACCESS_TOKEN
+  config.access_token_secret = MY_ACCESS_TOKEN_SECRET
+end
+
+user_screen_name = client.user.name
+
+puts("読み込み完了")
+
+# TL取得
+
+stream_client.user do |status|
+    if status.is_a?(Twitter::Tweet)
+      puts "#{status.user.name} (#{status.user.screen_name})"
+      puts status.text
+      puts '---'
+    end
+end
