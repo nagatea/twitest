39f1f03250e89946d43824aeff53ac25580313c2
nagatea <cheesecakebot@yahoo.co.jp>
2017-01-13 18:33:12 +0900

    指定したIDの最新のツイート1件をふぁぼるコード(fav.rb)の作成

---
+++ auto_retweet/fav.rb
@@ -0,0 +1,22 @@
+require "twitter"
+require "../mysystem.rb"
+
+ENV['SSL_CERT_FILE'] = File.expand_path('../cacert.pem')
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = MY_CONSUMER_KEY
+  config.consumer_secret     = MY_CONSUMER_SECRET
+  config.access_token        = MY_ACCESS_TOKEN
+  config.access_token_secret = MY_ACCESS_TOKEN_SECRET
+end
+puts("読み込み完了")
+
+if ARGV[0].to_s != "" 
+    puts "対象アカウント → @#{ARGV[0]}"
+    client.user_timeline(ARGV[0], { count: 1 } ).each do |timeline|
+        client.favorite(timeline.id)
+        puts "#{client.status(timeline.id).text} をふぁぼりました" 
+    end
+else
+    puts("[EROOR]IDをただしく入力してください")
+end
